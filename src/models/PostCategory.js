5a155d4b0f6a1d48c8e27233089a3d35da9b440b
NyPadilha <763npp@gmail.com>
2023-09-27 19:51:51 -0300

    feat: finish model PostCategory

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,6 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
     const PostCategory = sequelize.define('PostCategory', {
-        // ...
+        postId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        },
+        categoryId: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+        }
     }, {
         underscored: true,
         timestamps: false,
